6fcdcf7def29fdb3b0160d76995e8df0f85793ac
Felipe Martínez <pipernene@gmail.com>
2018-06-21 16:31:42 +0200

    Added Lobby plugin

---
+++ API Models/LolLobbyLobbyBotParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCU.NET.API_Models
+{
+    public class LolLobbyLobbyBotParams
+    {
+        /// <summary>
+        /// ['NONE', 'EASY', 'MEDIUM', 'HARD', 'UBER', 'TUTORIAL', 'INTRO']
+        /// </summary>
+        public string botDifficulty { get; set; }
+
+        public int championId { get; set; }
+        public string teamId { get; set; }
+    }
+}
---
+++ Plugins/LoL/Lobby.cs
@@ -0,0 +1,20 @@
+﻿using LCU.NET.API_Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LCU.NET.LeagueClient;
+
+namespace LCU.NET.Plugins.LoL
+{
+    public static class Lobby
+    {
+        /// <summary>
+        /// Adds a bot to a custom game lobby.
+        /// </summary>
+        public static void PostCustomBot(LolLobbyLobbyBotParams parameters)
+            => MakeRequest("/lol-lobby/v1/lobby/custom/bots", Method.POST, parameters);
+    }
+}
