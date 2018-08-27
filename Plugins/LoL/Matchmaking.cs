2eb1b2c8a57bc1606584fff57ba5ebdf6304b632
Felipe Martinez <pipernene@gmail.com>
2018-08-27 22:34:37 +0200

    Add ReadyCheckEndpoint

--- Plugins/LoL/Matchmaking.cs
+++ Plugins/LoL/Matchmaking.cs
@@ -10,15 +10,17 @@ namespace LCU.NET.Plugins.LoL
 {
     public static class Matchmaking
     {
-        [APIMethod("/lol-matchmaking/v1/ready-check", Method.GET)]
+        public const string ReadyCheckEndpoint = "/lol-matchmaking/v1/ready-check";
+
+        [APIMethod(ReadyCheckEndpoint, Method.GET)]
         public static Task<LolMatchmakingMatchmakingReadyCheckResource> GetReadyCheck()
             => MakeRequestAsync<LolMatchmakingMatchmakingReadyCheckResource>();
 
-        [APIMethod("/lol-matchmaking/v1/ready-check/accept", Method.POST)]
+        [APIMethod(ReadyCheckEndpoint + "/accept", Method.POST)]
         public static Task PostReadyCheckAccept()
             => MakeRequestAsync();
 
-        [APIMethod("/lol-matchmaking/v1/ready-check/decline", Method.POST)]
+        [APIMethod(ReadyCheckEndpoint + "/decline", Method.POST)]
         public static Task PostReadyCheckDecline()
             => MakeRequestAsync();
     }
